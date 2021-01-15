9748c3336e5b5e29091e4ed368353d06a0f32afd
saaaki527 <77159096+saaaki527@users.noreply.github.com>
2021-01-15 16:55:06 +0900

    Update main.js

--- 1_workspace/1/3の倍数/main.js
+++ 1_workspace/1/3の倍数/main.js
@@ -7,3 +7,19 @@ const genkiFunction = function(number) {
     }
     }
 }
+
+//FizzBuzz
+
+const FizzBuzzFunction = function(number) {
+  for (let n=1; n<=number; n++) {
+    if (n % 15 === 0) {
+      console.log(`FizzBuzz`);
+    } else if (n % 3 === 0) {
+      console.log(`Fizz`);
+    } else if (n % 5 === 0) {
+      console.log(`Buzz`);
+    } else {
+        console.log(n)
+      }
+  }
+}

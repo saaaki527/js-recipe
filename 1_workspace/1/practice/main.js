5f0f6a7c48903ce99c837cbe77d5d7f931cc2d23
saaaki527 <77159096+saaaki527@users.noreply.github.com>
2021-01-15 16:16:15 +0900

    Update main.js

--- 1_workspace/1/practice/main.js
+++ 1_workspace/1/practice/main.js
@@ -1,3 +1,15 @@
+//入力した値をコンソールに表示
+
+const inputText = document.getElementById('input-text')
+const inputDate = document.getElementById('input-date')
+
+const logValue = function (e) {
+  console.log(e.target.value)
+}
+
+inputText.oninput = logValue
+inputDate.oninput = logValue
+
 //押したキーの名前をコンソールに表示
 
 document.onkeydown = function (e) {
